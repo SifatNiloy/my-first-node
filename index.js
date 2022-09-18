df153907c458e895eb7292dfb3b526ed5e430a64
unknown <sifatniloy18@gmail.com>
2022-09-18 16:17:06 -0600

    installed nodemon

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('Look Mama! I can code now!!!')
-});
-
+    res.send('hello from my first node, installed nodemon, for auto restart')
+})
 app.listen(port, () => {
     console.log('listening to port', port)
 })
